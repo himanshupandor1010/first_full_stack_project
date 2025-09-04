6bcf1e3068b872c86734ef9f5edace4db52333ac
himanshupandor1010 <himanshupandor1010@gmail.com>
2025-09-04 18:43:28 +0530

    add logged in

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,43 +1,87 @@
 import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
+export const SignUpHandler = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-export const SignUpHandler =async (req,res)=>{
-   try {
-    const {username,email,password} = req.body;
-    
     const user = await User.findOne({
-     $or:[{username},{email}]
+      $or: [{ username }, { email }],
     }).select("-password");
+
+    if (user) {
+      if (user?.username === username) {
+        return res.status(400).json({
+          message: "Username already exist",
+        });
+      }
+      if (user?.email === email) {
+        return res.status(400).json({
+          message: "Email already exist",
+        });
+      }
+    }
+
+    const newUser = await User.create({ username, email, password });
+    console.log(newUser);
+    return res.status(201).json({
+      message: "User created succesfully",
+      user: {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        message: "Something went wrong while logged in",
+        error: error.message,
+      });
+  }
+};
+
+export const LoginHandler = async (req,res) => {
+  try {
+    const { NameOrEmail, password } = req.body;
+    const identifier = NameOrEmail;
+
+    const user = await User.findOne({
+      $or: [{ username: identifier }, { email: identifier }],
+    });
+
     
-    if(user)
-    {
-     if(user?.username===username)
-     {
-         return res.status(400).json({
-             message:"Username already exist"
-         })
-     }
-     if(user?.email===email)
-     {
-         return res.status(400).json({
-             message:"Email already exist"
-         })
-     }
-    } 
- 
-   const newUser = await User.create({username,email,password})
- 
-    res.status(201).json({
-     message:"User created succesfully",
-     user:{
-         _id:newUser._id,
-         username:newUser.username,
-         email:newUser.email
-     }
-    })
-   } catch (error) {
-    return res.status(500).json({ message: error.message });
-   }
-   
+
+    if (user) {
+        const isMatch = await bcrypt.compare(password,user.password)
+      if (isMatch) {
+        res.status(200).json({
+          message: "user logged in successfully",
+          user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      }
+        });
+      }
+      else{
+        res.status(401).json({
+            message:"Wrong Password"
+        })
+      }
+    } else {
+      res.status(404).json({
+        message: "User is not found",
+       
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong while logged in",
+      error: error.message, // valid here
     
-}
+    });
+  }
+};
+
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { SignUpHandler } from "../controllers/user.controller.js";
+import { LoginHandler, SignUpHandler } from "../controllers/user.controller.js";
 
 const router = express.Router()
 
 //SignUpRoute
 router.post("/signup",SignUpHandler);
+router.post("/login",LoginHandler);
 
 
 export default router;
